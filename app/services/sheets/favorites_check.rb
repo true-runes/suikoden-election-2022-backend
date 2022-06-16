7d3175e34bf517ae4cae5339af7a2c6316a3f49d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-17 00:06:33 +0900

    feat: 🎸 必要となるクラスをドラフトレベルであるが実装した (#21)

---
+++ app/services/sheets/favorites_check.rb
@@ -0,0 +1,12 @@
+module Sheets
+  class FavoritesCheck
+    def self.run
+      rows = SheetData.get_rows(
+        sheet_id: ENV.fetch('SUB_GENSOSENKYO_FAVORITES_CHECK_SHEET_ID'),
+        range: "お題小説!A2:H"
+      )
+
+      pp rows
+    end
+  end
+end
