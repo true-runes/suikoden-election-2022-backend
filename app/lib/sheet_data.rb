cf7c083da4ab209c86179a8ef9acba5159f98541
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-17 18:55:04 +0900

    feat: 🎸 Sheet に書き込みをするメソッドをリファクタリングした (#34)

--- app/lib/sheet_data.rb
+++ app/lib/sheet_data.rb
@@ -1,4 +1,6 @@
 class SheetData
+  VALUE_INPUT_OPTION = 'USER_ENTERED'.freeze
+
   def self.get_rows(sheet_id:, range:)
     service = SheetService.new.service
     response = service.get_spreadsheet_values(sheet_id, range)
@@ -13,5 +15,17 @@ class SheetData
     response = service.get_spreadsheet_values(sheet_id, range)
 
     response.range
+  end
+
+  def self.write_rows(sheet_id:, range:, values:)
+    service = SheetService.new.service
+
+    # 戻り値に updated_cells, updated_columns, updated_range, updated_rows メソッドが生えてる
+    service.update_spreadsheet_value(
+      sheet_id,
+      range, # 貼り付け開始フォーカス位置
+      Google::Apis::SheetsV4::ValueRange.new(values: values), # values は行列を示す二次元配列になる
+      value_input_option: VALUE_INPUT_OPTION
+    )
   end
 end
