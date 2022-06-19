de4a59d2806ab882e5f45c12ff54c9cad2439a9f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-20 08:36:31 +0900

    refactor: 💡 レスポンスを jbuilder で記述するようにした (#84)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'health_check', to: 'health_check#index'
-  get 'unite_attacks', to: 'unite_attacks#index'
-  get 'result_illustration_applications', to: 'result_illustration_applications#index'
+  get 'health_check', to: 'health_check#index', format: 'json'
+  get 'unite_attacks', to: 'unite_attacks#index', format: 'json'
+  get 'result_illustration_applications', to: 'result_illustration_applications#index', format: 'json'
 end
