4e6d509551a5d4e5f239010dfff7bd6bde38c67f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-20 08:33:31 +0900

    refactor: 💡 レスポンスを jbuilder で記述するようにした

--- app/controllers/unite_attacks_controller.rb
+++ app/controllers/unite_attacks_controller.rb
@@ -14,46 +14,12 @@ class UniteAttacksController < ApplicationController
       'woven' => '紡時'
     }[params[:title]]
 
-    attacks = OnRawSheetUniteAttack.where(sheet_name: convert_title_param_to_sheet_name)
+    @attacks = OnRawSheetUniteAttack.where(sheet_name: convert_title_param_to_sheet_name)
 
-    attacks = attacks.order(kana: :asc) if params[:order] == 'kana'
-
-    return_array = []
-    attacks.each do |attack|
-      return_hash = {
-        id: attack.id,
-        name: attack.name,
-        kana: attack.kana,
-        name_en: attack.name_en,
-        chara_1: attack.chara_1,
-        chara_2: attack.chara_2,
-        chara_3: attack.chara_3,
-        chara_4: attack.chara_4,
-        chara_5: attack.chara_5,
-        chara_6: attack.chara_6,
-        page_annotation: attack.page_annotation,
-        character_names: character_names(attack)
-      }
-
-      return_array.push(return_hash)
-    end
-
-    render json: return_array.to_json
+    @attacks = @attacks.order(kana: :asc) if params[:order] == 'kana'
   end
 
   private
-
-  # いったん愚直に
-  def character_names(attack)
-    character_names = attack.chara_1
-    character_names = "#{character_names}＆#{attack.chara_2}" if attack.chara_2.present?
-    character_names = "#{character_names}＆#{attack.chara_3}" if attack.chara_3.present?
-    character_names = "#{character_names}＆#{attack.chara_4}" if attack.chara_4.present?
-    character_names = "#{character_names}＆#{attack.chara_5}" if attack.chara_5.present?
-    character_names = "#{character_names}＆#{attack.chara_6}" if attack.chara_6.present?
-
-    character_names
-  end
 
   def all_unite_attacks
     return_hash = {}
@@ -79,7 +45,7 @@ class UniteAttacksController < ApplicationController
           id: attack.id,
           name: attack.name,
           name_en: attack.name_en,
-          character_names: character_names(attack),
+          character_names: Presenter::UniteAttacks.character_names(attack),
           page_annotation: attack.page_annotation
         }
 
---
+++ app/views/unite_attacks/index.json.jbuilder
@@ -0,0 +1,14 @@
+json.array! @attacks do |attack|
+  json.id attack.id
+  json.name attack.name
+  json.kana attack.kana
+  json.name_en attack.name_en
+  json.chara_1 attack.chara_1
+  json.chara_2 attack.chara_2
+  json.chara_3 attack.chara_3
+  json.chara_4 attack.chara_4
+  json.chara_5 attack.chara_5
+  json.chara_6 attack.chara_6
+  json.page_annotation attack.page_annotation
+  json.characters Presenter::UniteAttacks.character_names(attack)
+end
