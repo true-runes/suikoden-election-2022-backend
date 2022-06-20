a6ecb37471686ef8a7d9528eb1fcebbecf6b15ff
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-20 23:37:43 +0900

    feat: 🎸 投票チェックAPIのひな形を追加した

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'health_check', to: 'health_check#index', format: 'json'
   get 'unite_attacks', to: 'unite_attacks#index', format: 'json'
   get 'result_illustration_applications', to: 'result_illustration_applications#index', format: 'json'
+  get 'check_vote', to: 'check_vote#index', format: 'json'
 end
