c7884147b872edc1a5e1717d343fabbca34bc9d9
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-22 22:25:54 +0900

    feat: 🎸 キャラクターの抽出サービスを追加した

--- app/lib/cloud_language.rb
+++ app/lib/cloud_language.rb
@@ -1,8 +1,3 @@
-# c = CloudLanguage.client; document = { type: :PLAIN_TEXT, content: '吾輩は猫である。名前はまだ無い。' }
-# result = c.analyze_syntax(document: document)
-# result.tokens.size
-# result.tokens[1]
-
 class CloudLanguage
   def self.client(credentials_path: nil)
     Google::Cloud::Language.language_service do |config|
