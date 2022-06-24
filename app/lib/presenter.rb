66ce0e107adb7568396217b923586a3bf7203c0b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-24 16:37:03 +0900

    feat: 🎸 協力攻撃APIが返すキャラ名の並びを sort した (#160)

--- app/lib/presenter.rb
+++ app/lib/presenter.rb
@@ -1,15 +1,20 @@
 module Presenter
   class UniteAttacks
-    # いったん愚直に
     def self.character_names(attack)
-      character_names = attack.chara_1
-      character_names = "#{character_names}＆#{attack.chara_2}" if attack.chara_2.present?
-      character_names = "#{character_names}＆#{attack.chara_3}" if attack.chara_3.present?
-      character_names = "#{character_names}＆#{attack.chara_4}" if attack.chara_4.present?
-      character_names = "#{character_names}＆#{attack.chara_5}" if attack.chara_5.present?
-      character_names = "#{character_names}＆#{attack.chara_6}" if attack.chara_6.present?
+      character_names = [
+        attack.chara_1,
+        attack.chara_2,
+        attack.chara_3,
+        attack.chara_4,
+        attack.chara_5,
+        attack.chara_6,
+      ]
 
-      character_names
+      character_names.compact_blank!
+      character_names.compact! # これはもはや不要っぽい
+      character_names.sort!
+
+      character_names.join('＆')
     end
   end
 
