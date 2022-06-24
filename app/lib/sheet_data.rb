5deff6cd7513a002ee7d21404cecf1fd4ba17e5f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-24 13:22:26 +0900

    docs: ✏️ コメントを微追加 (#156)

--- app/lib/sheet_data.rb
+++ app/lib/sheet_data.rb
@@ -21,10 +21,11 @@ class SheetData
     service = SheetService.new.service
 
     # 戻り値に updated_cells, updated_columns, updated_range, updated_rows メソッドが生えてる
+    # range は A1 形式しか選択できない (https://googleapis.dev/ruby/google-api-client/latest/Google/Apis/SheetsV4/SheetsService.html#update_spreadsheet_value-instance_method)
     service.update_spreadsheet_value(
       sheet_id,
       range, # 貼り付け開始フォーカス位置
-      Google::Apis::SheetsV4::ValueRange.new(values: values), # values は行列を示す二次元配列になる
+      Google::Apis::SheetsV4::ValueRange.new(values: values), # values は行列を示す二次元配列になることに注意する
       value_input_option: VALUE_INPUT_OPTION
     )
   end
