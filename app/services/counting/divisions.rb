40da7b8c27f3b691ece419d95257b11bcd3ad18b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-25 19:07:00 +0900

    feat: 🎸 集計用のサービスクラスの雛形の雛形を作成した (#177)

---
+++ app/services/counting/divisions.rb
@@ -0,0 +1,21 @@
+module Counting
+  class Divisions
+    def self.voted_both_divisions
+      # これは User に生えるメソッドな気がする
+      # counting_xxxxs なテーブルを作ってからリファクタ
+      # counting_xxxxs には取得漏れも入れることができる（その際は明示的にファイルにして入れる）
+    end
+
+    def self.voted_only_all_characters_division
+      # これは User に生えるメソッドな気がする
+      # counting_xxxxs なテーブルを作ってからリファクタ
+      # counting_xxxxs には取得漏れも入れることができる（その際は明示的にファイルにして入れる）
+    end
+
+    def self.voted_only_unite_attacks_division
+      # これは User に生えるメソッドな気がする
+      # counting_xxxxs なテーブルを作ってからリファクタ
+      # counting_xxxxs には取得漏れも入れることができる（その際は明示的にファイルにして入れる）
+    end
+  end
+end
---
+++ app/services/counting/languages.rb
@@ -0,0 +1,15 @@
+module Counting
+  class Languages
+    def self.what_languages_all_characters_division_voted
+      # これは Tweet に生えるメソッドな気がする
+      # counting_xxxxs なテーブルを作ってからリファクタ
+      # counting_xxxxs には取得漏れも入れることができる（その際は明示的にファイルにして入れる）
+    end
+
+    def self.what_languages_unite_attacks_division_voted
+      # これは Tweet に生えるメソッドな気がする
+      # counting_xxxxs なテーブルを作ってからリファクタ
+      # counting_xxxxs には取得漏れも入れることができる（その際は明示的にファイルにして入れる）
+    end
+  end
+end
