46f581a633108ce3b327e9768294990e0bbdcaef
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-26 21:05:01 +0900

    feat: 🎸 Clasp のスクリプトを更新した v2 (#187)

--- clasp/gensosenkyo/ZzzConditionalFormats.ts
+++ clasp/gensosenkyo/ZzzConditionalFormats.ts
@@ -4,21 +4,37 @@
 // https://bamka.info/docs-google-gyo-irokae/
 
 namespace ZzzConditionalFormats {
-  export const setRedBackgroundWhenTrue = (range: GoogleAppsScript.Spreadsheet.Range, sheet: GoogleAppsScript.Spreadsheet.Sheet) => {
-    ZzzConditionalFormats.setColorToRangeInSpecificCondition(
-      range,
-      sheet,
-      'TRUE',
-      '#ffc0cb'
-    )
-  }
-
-  export const setColorToRangeInSpecificCondition = (range: GoogleAppsScript.Spreadsheet.Range, sheet: GoogleAppsScript.Spreadsheet.Sheet, conditionValue: string, colorCode: string) => {
+  export const setColorToRangeInSpecificCondition = (
+    range: GoogleAppsScript.Spreadsheet.Range,
+    sheet: GoogleAppsScript.Spreadsheet.Sheet,
+    conditionValue: string,
+    colorCode: string
+  ) => {
     const newRule = SpreadsheetApp
       .newConditionalFormatRule()
-      .whenTextEqualTo(conditionValue)
-      .setBackground(colorCode)
-      .setRanges([range])
+      .whenTextEqualTo(conditionValue) // この条件が満たされる時に（セル無指定の場合は条件が判定されるセルは range に等しい）、
+      .setBackground(colorCode) // このメソッドが適用されて、
+      .setRanges([range]) // 適用範囲は range になる（getRange した値を配列にくるんで渡す）
+      .build()
+
+    // 既存のルールに追加する
+    const rules = sheet.getConditionalFormatRules()
+    rules.push(newRule)
+
+    sheet.setConditionalFormatRules(rules)
+  }
+
+  export const setGrayBackgroundToAllRowCellsInSpecificCondition  = (
+    rowNumber: number,
+    sheet: GoogleAppsScript.Spreadsheet.Sheet,
+    formulaSatisfied: string
+  ) => {
+    // formulaSatisfied は '=$F$2=FALSE' などのような形の条件文になる
+    const newRule = SpreadsheetApp
+      .newConditionalFormatRule()
+      .whenFormulaSatisfied(formulaSatisfied) // この条件が満たされる時に、
+      .setBackground('#a9a9a9') // このメソッドが適用されて、
+      .setRanges([sheet.getRange(rowNumber, 1, 1, 50)]) // 適用範囲は range になる（getRange した値を配列にくるんで渡す）
       .build()
 
     // 既存のルールに追加する
--- clasp/gensosenkyo/ZzzDataValidation.ts
+++ clasp/gensosenkyo/ZzzDataValidation.ts
@@ -1,3 +1,4 @@
+// 「入力規則」
 namespace ZzzDataValidation {
   export const setDataValidationToCell = (sheetName: string, columnNumber: number, destroyWord: string = '') => {
     const sheet = ZzzSheetOperations.changeActiveSheetTo(sheetName)
@@ -18,12 +19,42 @@ namespace ZzzDataValidation {
     }
   }
 
-  export const rangeWhichContainsDataList = (row_number: number, sheet: GoogleAppsScript.Spreadsheet.Sheet) => {
+  export const rangeWhichContainsDataList = (rowNumber: number, sheet: GoogleAppsScript.Spreadsheet.Sheet) => {
     // AXn (50列目) を始点として、GRn (200列目)までの列で、行は n行目 である範囲
     // 第3引数 および 第4引数 には注意が必要で、自分自身を含めた個数を指定する
     // したがって 第4引数 の値は 151 になる
     // cf. https://takamin.github.io/techtips/xlsColNumMap
 
-    return sheet.getRange(row_number, 50, 1, 151)
+    return sheet.getRange(rowNumber, 50, 1, 151)
   }
+
+  export const setDataValidationDirectMessageTypes = (sheetName: string, columnNumber: number, destroyWord: string = '') => {
+    const sheet = ZzzSheetOperations.changeActiveSheetTo(sheetName)
+    const startRowNumber = 2
+    const endRowNumber = 101
+
+    for (let i = startRowNumber ; i <= endRowNumber ; i++) {
+      const dataValidationTargetCell = sheet.getRange(i, columnNumber, 1, 1)
+
+      if (destroyWord === 'DESTROY') { // この引数が与えられていると入力規則全削除
+        dataValidationTargetCell.setDataValidation(null)
+      } else {
+        const requiredValues = ZzzDataValidation.directMessageTypes
+        const rule = SpreadsheetApp.newDataValidation().requireValueInList(requiredValues).build()
+
+        dataValidationTargetCell.setDataValidation(rule)
+      }
+    }
+  }
+
+  export const directMessageTypes = [
+    '①オールキャラ部門',
+    '②協力攻撃部門',
+    'ボ・OP・CLイラスト',
+    'ボ・お題小説',
+    'ボ・開票イラスト',
+    'ボ・推し台詞',
+    'ボ・選挙運動',
+    'その他',
+  ]
 }
