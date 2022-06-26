ad6b3a688b27b49ac3d33af822014203ec755f43
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-26 09:22:58 +0900

    feat: 🎸 AnalyzeSyntax をリファクタリングして実行時間および料金を抑えた (#179)

--- app/services/natural_language/suggest_character_names.rb
+++ app/services/natural_language/suggest_character_names.rb
@@ -1,7 +1,9 @@
 module NaturalLanguage
   class SuggestCharacterNames
     def self.exec(tweet_or_dm)
-      analyze_syntax = tweet_or_dm.analyze_syntax
+      analyze_syntax = tweet_or_dm&.analyze_syntax
+
+      return ['解析が未実行です'] if analyze_syntax.blank?
 
       PickupCharacterNames.exec(analyze_syntax)
     end
