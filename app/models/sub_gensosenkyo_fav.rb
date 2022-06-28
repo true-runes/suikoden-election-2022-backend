8de3e8e9f14d782b069a514769a3fb06cf5f8be4
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-29 07:09:10 +0900

    refactor: 💡 id_number だけで URL を生成するメソッドのメソッド名をリファクタリングした

--- app/models/sub_gensosenkyo_fav.rb
+++ app/models/sub_gensosenkyo_fav.rb
@@ -1,7 +1,7 @@
 class SubGensosenkyoFav < ApplicationRecord
   belongs_to :tweet, optional: true
 
-  def url_on_id_number
+  def url_as_screen_name_is_twitter
     "https://twitter.com/twitter/status/#{id_number}"
   end
 end
