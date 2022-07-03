0b37b3cf4587bd25bf4a67bf74ba9389151cbc93
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-07-03 14:39:23 +0900

    feat: 🎸 マイグレーションを見直し、ツイートとDMはenumで区別することにした (#214)

--- app/models/counting_all_character.rb
+++ app/models/counting_all_character.rb
@@ -1,6 +1,11 @@
 class CountingAllCharacter < ApplicationRecord
+  belongs_to :tweet, optional: true
+  belongs_to :direct_message, optional: true
+
   scope :invisible, -> { where(is_invisible: true) }
   scope :valid_tweets, -> { where(is_out_of_counting: false).where(is_invisible: false) }
+
+  enum vote_method: { by_tweet: 0, by_direct_message: 1, by_others: 99 }
 
   def self.tweets_whose_invisible_status_is_different_between_sheet_and_database
     sheet_invisible_tweet_ids = CountingAllCharacter.invisible.pluck(:tweet_id)
