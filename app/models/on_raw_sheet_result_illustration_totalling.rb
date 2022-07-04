c18a122f2a4b1588106ea4c993589fbee4165a25
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-07-04 20:34:03 +0900

    feat: 🎸 開票イラストシートのキャラ名とDBのキャラ名の相違を出力するメソッドを追加した

--- app/models/on_raw_sheet_result_illustration_totalling.rb
+++ app/models/on_raw_sheet_result_illustration_totalling.rb
@@ -1,13 +1,14 @@
 class OnRawSheetResultIllustrationTotalling < ApplicationRecord
-  # on_raw_sheet_result_illustration_totalling
-  def convert_name_to_gensosenkyo_style(name)
-    {
-      "ほげ" => "ほげげげ"
-    }[name] || name
+  def self.not_exists_character_names_in_db
+    on_sheet_character_names = OnRawSheetResultIllustrationTotalling.pluck(:character_name_for_public)
+    on_db_character_names = Character.pluck(:name)
+
+    not_exists_character_names_in_db = []
+
+    on_sheet_character_names.each do |character_name|
+      not_exists_character_names_in_db << character_name unless character_name.in?(on_db_character_names)
+    end
+
+    not_exists_character_names_in_db
   end
 end
-
-# irb(main):003:0> x = { a: 1, b: 2, c: 3 }
-# irb(main):004:0> y = { a: 4, c: 5, d: 10 }
-# irb(main):007:0> x.merge(y) { |_, oldval, newval| oldval + newval }
-# => {:a=>5, :b=>2, :c=>8, :d=>10}
