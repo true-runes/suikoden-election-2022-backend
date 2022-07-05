1918ee956b41f43c731f6d61b45beb0db33cc335
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-07-05 09:07:49 +0900

    feat: 🎸 最終集計まわりをリファクタ v7

---
+++ app/services/counting/checkers/bonus_votes.rb
@@ -0,0 +1,10 @@
+module Counting
+  module Checkers
+    class BonusVotes
+      def self.vote_to_the_same_characters_users
+        # TODO: 書く
+        # 票数に影響はない
+      end
+    end
+  end
+end
---
+++ app/services/counting/checkers/unite_attacks.rb
@@ -0,0 +1,9 @@
+module Counting
+  module Checkers
+    class UniteAttacks
+      def self.words_which_not_exist_database
+        # TODO: 書く
+      end
+    end
+  end
+end
